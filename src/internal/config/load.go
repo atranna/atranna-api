e1e4f0b20a4fadfd8b5bd2e86dbc981484193365
DontDDoS <git@dontddos.me>
2026-08-02 01:21:48 +0200

    Standardized environment variable names

--- src/internal/config/load.go
+++ src/internal/config/load.go
@@ -11,6 +11,6 @@ func Load() {
 	Current.Debug = getEnvAsBool("DEBUG", defaultConfig.Debug)
 	Current.Auth.Enable = getEnvAsBool("AUTH_ENABLE", defaultConfig.Auth.Enable)
 	Current.Auth.MasterToken = getEnv("AUTH_MASTER_TOKEN", defaultConfig.Auth.MasterToken)
-	Current.Service.Port = getEnv("PORT", defaultConfig.Service.Port)
+	Current.Service.Port = getEnv("SERVICE_PORT", defaultConfig.Service.Port)
 	Current.Storage.Backend = getEnv("STORAGE_BACKEND", defaultConfig.Storage.Backend)
 }
