afdcc40f15b3c614f4b2492bcc5ddc84cd5992ed
DontDDoS <git@dontddos.me>
2026-08-02 16:08:37 +0200

    Implement SQLite migrations

--- src/internal/database/sqliteMigrations.go
+++ src/internal/database/sqliteMigrations.go
@@ -1,7 +1,25 @@
 package database
 
+import (
+	"errors"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite"
+)
+
 
 func ApplySQLiteMigrations() {
-	
-	
+	driver, err := sqlite.WithInstance(DB, &sqlite.Config{})
+	if err != nil {
+		panic(err)
+	}	
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://etc/atranna-api/migrations/sqlite",
+		"sqlite3", driver)
+	if err != nil {
+		panic(err)
+	}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		panic(err)
+	}
 }
