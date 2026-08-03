1b3e13a43dd9d27042e5e5e89152a2d3f268d029
DontDDoS <git@dontddos.me>
2026-08-03 22:19:15 +0200

    Block master token from accessing /api/v1/users/me

--- src/internal/router/router.go
+++ src/internal/router/router.go
@@ -93,7 +93,7 @@ func New() *gin.Engine {
 	usersGroup.GET("/:id", usersHandler.GetUser)
 	usersGroup.POST("", usersHandler.Add)
 	usersGroup.DELETE("/:id", usersHandler.Delete)
-	usersGroup.GET("/me", usersHandler.GetSelf)
+	usersGroup.GET("/me", usersHandler.GetSelf, middlewares.BlockMasterTokenMiddleware())
 
 	// Auth
 	authGroup := apiV1.Group("/auth")
