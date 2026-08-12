82adea6266a47fe5fcbd27eb7e9d135cab2405a9
DontDDoS <git@dontddos.me>
2026-08-12 17:10:51 +0200

    Update system role permissions
    Replace string literals with permission constants in `SystemRoles`, leave `owner` unconstrained, and grant `admin` read/write access to roles.

--- src/internal/auth/systemRoles.go
+++ src/internal/auth/systemRoles.go
@@ -1,8 +1,8 @@
 package auth
 
 var SystemRoles = map[string][]PermissionKey{
-	"owner":  { "devices:read", "devices:write", "interfaces:read", "interfaces:write", "networks:read", "networks:write", "organization:read", "organization:write", "users:read", "users:write", "organization_members:read", "organization_members:write" },
-	"admin":  { "devices:read", "devices:write", "interfaces:read", "interfaces:write", "networks:read", "networks:write", "organization:read", "users:read", "users:write", "organization_members:read", "organization_members:write" },
-	"operator":   { "devices:read", "devices:write", "interfaces:read", "interfaces:write", "networks:read", "networks:write", "organization:read", "users:read" },
-	"viewer": { "devices:read", "interfaces:read", "networks:read", "organization:read", "users:read" },
+	"owner":  {}, // Has permission to do anything, so no need to list permissions here
+	"admin":  { DevicesRead, DevicesWrite, InterfacesRead, InterfacesWrite, NetworksRead, NetworksWrite, OrganizationRead, OrganizationWrite, OrganizationMembersRead, OrganizationMembersWrite, RolesRead, RolesWrite },
+	"operator":   { DevicesRead, DevicesWrite, InterfacesRead, InterfacesWrite, NetworksRead, NetworksWrite, OrganizationRead, UsersRead },
+	"viewer": { DevicesRead, InterfacesRead, NetworksRead, OrganizationRead, UsersRead },
 }
