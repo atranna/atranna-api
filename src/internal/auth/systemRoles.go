969e41934949bae4d2dbcfea343e59a1661517e6
DontDDoS <git@dontddos.me>
2026-08-15 10:11:51 +0200

    Add organization_memebers:read to viewers

--- src/internal/auth/systemRoles.go
+++ src/internal/auth/systemRoles.go
@@ -4,5 +4,5 @@ var SystemRoles = map[string][]PermissionKey{
 	"owner":  {}, // Has permission to do anything, so no need to list permissions here
 	"admin":  { DevicesRead, DevicesWrite, InterfacesRead, InterfacesWrite, NetworksRead, NetworksWrite, OrganizationRead, OrganizationWrite, OrganizationMembersRead, OrganizationMembersWrite, RolesRead, RolesWrite },
 	"operator":   { OrganizationMembersRead, DevicesRead, DevicesWrite, InterfacesRead, InterfacesWrite, NetworksRead, NetworksWrite, OrganizationRead, UsersRead },
-	"viewer": { DevicesRead, InterfacesRead, NetworksRead, OrganizationRead, UsersRead },
+	"viewer": { OrganizationMembersRead, DevicesRead, InterfacesRead, NetworksRead, OrganizationRead, UsersRead },
 }
